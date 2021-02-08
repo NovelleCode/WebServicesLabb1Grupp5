d14fc2917c810f70f0154d7e15727fd32aa87299
Helena <helenalundstrom@hotmail.se>
2021-02-08 14:45:47 +0100

    Changed stuff

--- core/src/main/java/com/webservices/HTTPServer.java
+++ core/src/main/java/com/webservices/HTTPServer.java
@@ -13,6 +13,7 @@ public class HTTPServer {
         HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 8001), 0);
         server.createContext("/index.html", new  MyHttpHandler());
         server.createContext("/cat.png", new  MyHttpHandler());
+        server.createContext("/sheet.pdf", new MyHttpHandler());
         server.createContext("/names", new NamesHttpHandler());
 //        server.createContext("/index.html", new  MyHttpHandler());
         ExecutorService executorService = Executors.newCachedThreadPool();
