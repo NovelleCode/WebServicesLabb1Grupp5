d3790f6d68611318afe9600b0a8d6e6000dcfda3
Karen Batjes <batjeskaren@gmail.com>
2021-02-11 14:22:43 +0100

    Restructured DatabaseHttpHandler

--- core/src/main/java/com/webservices/httphandler/DatabaseHttpHandler.java
+++ core/src/main/java/com/webservices/httphandler/DatabaseHttpHandler.java
@@ -18,47 +18,43 @@ public class DatabaseHttpHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String getURL = null;
-        System.out.println(exchange.getRequestMethod());
-        if ("GET".equals(exchange.getRequestMethod())) {
-            //getURL = exchange.getRequestURI().toString().substring(1);
-            System.out.println(exchange.getRequestURI());
-        } else if ("POST".equals(exchange.getRequestMethod())) {
-            //  getURL = exchange.getRequestURI().toString().substring(1) + ".html";
-            System.out.println(exchange.getRequestURI());
-
+        String requestMethod = exchange.getRequestMethod();
+        switch(requestMethod) {
+            case "HEAD":
+                handleHeadResponse(exchange);
+                // M1 : Only header information as a response
+                break;
+            case "GET":
+                handleGetResponse(exchange);
+                // M1 :Header information as a response ++++
+                // M2 :Response body
+                break;
+            case "POST":
+                // M1 :Only header information as a response ++++
+                // M2 :Response body
+                // M3. Prepare post request information cq: take in firstname, lastname, create person, add person to table
+                handlePostResponse(exchange);
+                break;
+            default:
+                // something here too?
+                break;
         }
-        handleGetResponse(exchange, getURL);
-        System.out.println(getURL);
     }
 
-    private static void handleGetResponse(HttpExchange exchange, String getURL) throws IOException {
+    private void handleHeadResponse(HttpExchange exchange) {
+    }
 
-        BufferedReader httpInput = new BufferedReader(new InputStreamReader(exchange.getRequestBody(), "UTF-8"));
-        StringBuilder in = new StringBuilder();
-        String input;
-        while ((input = httpInput.readLine()) != null) {
-            in.append(input).append(" ");
-        }
-        if (exchange.getRequestMethod().equals("POST")) {
-            System.out.println(in);
-            String body = in.toString();
-            String fName = StringUtils.substringBetween(body, "fname=", "&");
-            String lname = StringUtils.substringAfter(body, "lname=").trim();
 
-            System.out.println("Firstname:" + fName);
-            System.out.println("Lastname:" + lname);
-
-            Person p = new Person(fName, lname);
-            pdao.create(p);
-
-        }
-
-        httpInput.close();
+    private static void handlePostResponse(HttpExchange exchange) throws IOException {
+        String body = getFirstNameLastNameInput(exchange);
+        String fName = getFirstName(body);
+        String lName = getLastName(body);
+        createAndAddPerson(fName, lName);
 
         String json = createJsonResponse();
         OutputStream outputStream = exchange.getResponseBody();
         exchange.getResponseHeaders().set("Content-Type", "application/json");
+        exchange.getResponseHeaders().set("Content-Length", String.valueOf(json.length()));
         exchange.sendResponseHeaders(200, json.length());
 
         outputStream.write(json.getBytes());
@@ -66,18 +62,54 @@ public class DatabaseHttpHandler implements HttpHandler {
         outputStream.close();
     }
 
+    private static void handleGetResponse(HttpExchange exchange) throws IOException {
+
+
+
+        String json = createJsonResponse();
+        OutputStream outputStream = exchange.getResponseBody();
+        exchange.getResponseHeaders().set("Content-Type", "application/json");
+        exchange.getResponseHeaders().set("Content-Length", String.valueOf(json.length()));
+        exchange.sendResponseHeaders(200, json.length());
+
+        outputStream.write(json.getBytes());
+        outputStream.flush();
+        outputStream.close();
+    }
+
+    private static void createAndAddPerson(String fName, String lName) {
+        Person p = new Person(fName, lName);
+        pdao.create(p);
+    }
+
+    private static String getFirstNameLastNameInput(HttpExchange exchange) throws IOException {
+        BufferedReader httpInput = new BufferedReader(new InputStreamReader(exchange.getRequestBody(), "UTF-8"));
+        StringBuilder in = new StringBuilder();
+        String input;
+        while ((input = httpInput.readLine()) != null) {
+            in.append(input).append(" ");
+        }
+        httpInput.close();
+        return in.toString();
+    }
+
+    private static String getLastName(String body) {
+        String lname = StringUtils.substringAfter(body, "lname=").trim();
+        return lname;
+    }
+
+    private static String getFirstName(String body) {
+        String fName = StringUtils.substringBetween(body, "fname=", "&");
+        return fName;
+    }
+
     private static String createJsonResponse() {
-
         var list = pdao.getAll();
-
         JsonConverter converter = new JsonConverter();
-
         var json = converter.convertToJson(list);
         System.out.println(json);
         return json;
     }
-
-
 
 
 }
