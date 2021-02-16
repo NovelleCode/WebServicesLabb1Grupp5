c9dc22409f472b0dcd46bcb1a1234848d81d8449
Karen Batjes <batjeskaren@gmail.com>
2021-02-16 14:55:29 +0100

    Cleaned up some more, and created blueprint for PluginHttpHandler

--- database/src/main/java/com/webservices/models/PersonDAO.java
+++ database/src/main/java/com/webservices/models/PersonDAO.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface PersonDAO {
     void createAndAddPerson (String firstName, String lastName);
-    // Inserts firstName & lastName in new person and add it to the database
+    // Inserts firstName & lastName in new person and adds it to the database
 
     List<Person> getAll();
     // Returns a list of all Persons in database
