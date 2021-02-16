688f5725afc1a0406d3535555353c4ef9e96d251
Karen Batjes <batjeskaren@gmail.com>
2021-02-16 21:11:33 +0100

    Made a start in PluginHttpHandler

--- plugin/src/main/java/com/webservices/plugin/PluginHttpHandler.java
+++ plugin/src/main/java/com/webservices/plugin/PluginHttpHandler.java
@@ -3,7 +3,10 @@ package com.webservices.plugin;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
 
 @Route("/easteregg")
 public class PluginHttpHandler implements HttpHandler {
@@ -12,10 +15,10 @@ public class PluginHttpHandler implements HttpHandler {
     public void handle(HttpExchange exchange) throws IOException {
         switch (exchange.getRequestMethod()) {
             case "HEAD":
-                //handleHeaderResponse(exchange);
+                handleHeaderResponse(exchange);
                 break;
             case "GET":
-                //handleHeaderResponse(exchange);
+                handleHeaderResponse(exchange);
                 //handleBodyResponse(exchange);
                 // Load some funny image or some javascript function
                 break;
@@ -24,5 +27,24 @@ public class PluginHttpHandler implements HttpHandler {
                 System.out.println(exchange.getRequestMethod());
                 break;
         }
+    }
+
+    private void handleHeaderResponse(HttpExchange exchange) throws IOException {
+        File file = new File("files" + File.separator + "easteregg");
+        String contentType = Files.probeContentType(file.toPath());
+        exchange.getResponseHeaders().set("Content-Type: ", contentType);
+        exchange.getResponseHeaders().set("Content-Length: ", String.valueOf(file.length()));
+        exchange.sendResponseHeaders(200, file.length());
+    }
+
+    private void handleBodyResponse(HttpExchange exchange) throws IOException {
+        OutputStream outputStream = exchange.getResponseBody();
+
+        File file = new File("files" + File.separator + "easteregg");;
+//        byte[] page = FileReader.readFromFile(file);
+
+//        outputStream.write(page);
+        outputStream.flush();
+        outputStream.close();
     }
 }
